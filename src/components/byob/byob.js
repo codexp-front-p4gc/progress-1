205ebfba9e30f0698d904515ed1a9df2b0d496cd
SHIRLEY ANA OVANDO PAZ <37024731818@jpmorgan.sp>
2017-11-29 20:35:11 -0200

    Byob JS e CSS Atualizado

--- src/components/byob/byob.js
+++ src/components/byob/byob.js
@@ -1,17 +1,15 @@
 import React from "react";
-import byobIcons from "../byob-icons/ByobIcons";
-import byobForm from "../byob-form/byob-form";
+import ByobIcons from "../byob-icons/ByobIcons";
+import ByobForm from "../byob-form/byob-form";
+import Description from "../description/description";
 import "./byob.css"
 
 export default (props) => (
     <section class="byob">
         <h1 class="byob__title">{props.title}</h1>
-        <byobIcons />
-        <article class="description">
-            <h2 class="description__title description__title--lighter">{props.subtitle}}</h2>
-            <p class="description__paragraph">{props.text}</p>
-        </article>
-        <byobForm />
+        <ByobIcons />
+        <Description />
+        <ByobForm />
     </section>
 );
 
