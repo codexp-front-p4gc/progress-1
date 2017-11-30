deab66a885a6dd8eca279cfabcc5d84d4aaeb0bd
SHIRLEY ANA OVANDO PAZ <37024731818@jpmorgan.sp>
2017-11-30 20:52:43 -0200

    Consolidado JS

--- src/components/byob/byob.js
+++ src/components/byob/byob.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ByobIcons from "../byob-icons/ByobIcons";
 import ByobForm from "../byob-form/byob-form";
-import Description from "../description/description";
+import Description from "../Description/Description";
 import "./byob.css"
 
 export default (props) => (
     <section class="byob">
         <h1 class="byob__title">{props.title}</h1>
         <ByobIcons />
-        <Description />
-        <ByobForm />
+        <Description title="Flexible Platform" paragraph="We are and industry agnostic platform. If you can track it, you can Progress it! Progress makes it easy for service providers to connect directly with their customers."/>
+        <ByobForm description="Invite Me" email="Add your mail here"/>
     </section>
 );
-
